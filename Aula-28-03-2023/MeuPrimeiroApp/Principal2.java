d7beb1aaaa0746e3ffaa406f237d92224f18f122
rafonpe <rafonpe35@gmail.com>
2023-03-28 22:49:47 -0300

    comentario

---
+++ Aula-28-03-2023/MeuPrimeiroApp/Principal2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Principal2{
+
+	public static void main(String[] args){
+		
+	System.out.println("########Cadastro de Aluno########");
+
+	System.out.println("Digite o seu nome:");
+	
+	Scanner entradaDados = new Scanner(System.in);
+
+	String nome = entradaDados.next();
+
+	System.out.println("O nome digitado foi: " );
+      System.out.println(nome);
+		
+
+	}
+
+
+}
+
